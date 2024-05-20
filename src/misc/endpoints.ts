5e8efbea17c33af1fb302bb56a7f09675215577f
Yuan_Lu <642649909@qq.com>
2024-05-20 16:59:04 +0300

    Add enpoints urls.

---
+++ src/misc/endpoints.ts
@@ -0,0 +1,3 @@
+const localhost = "http://localhost:5074/api/v1/";
+export const topRatedProduct = `${localhost}products/most_purchased/`;
+export const mostPurchasedProduct = `${localhost}products/most_purchased/`;
